e33ec610b99ea1a47bdeec79d4225e38eed8ea64
techielukesh <alltechfactzzz@gmail.com>
2026-05-23 19:10:22 +0530

    all error fixed in this commit

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
-import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from 'sonner'
+import ClerkProviderClient from "@/components/ClerkProviderClient";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
@@ -12,30 +12,25 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${inter.className}`}
-      >
-        {/* header */}
-        <Header/>
+    <ClerkProviderClient>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          {/* header */}
+          <Header />
 
-        
-        <main className="min-h-screen">
-        <Toaster richColors/>
-        
-          {children}
+          <main className="min-h-screen">
+            <Toaster richColors />
+
+            {children}
           </main>
-        {/* footer */}
-        <footer className="bg-blue-50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>
-              Made by me 
-            </p>
-         </div>
-        </footer>
-      </body>
+          {/* footer */}
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made by me</p>
+            </div>
+          </footer>
+        </body>
       </html>
-      </ClerkProvider>
+    </ClerkProviderClient>
   );
 }
